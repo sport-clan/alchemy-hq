c55b2a12dc161acad53627ebd48dabea2acbacea
James Pharaoh <james@phsys.co.uk>
2011-06-22 17:20:54 +0200

    allow nesting prop and comment inside section in php-ini format

--- ruby/mandar/support/php.rb
+++ ruby/mandar/support/php.rb
@@ -62,24 +62,41 @@ module Mandar::Support::PHP
 
 	def self.format_php_ini(file_elem, f)
 
-		file_elem.find("*").each do |elem|
-			case elem.name
+		file_elem.find("*").each do |elem0|
+			case elem0.name
 
 			when "section"
-				section_name = elem.attributes["name"]
+				section_name = elem0.attributes["name"]
 				f.print "[#{section_name}]\n"
 
+				elem0.find("*").each do |elem1|
+					case elem1.name
+
+					when "prop"
+						prop_name = elem1.attributes["name"]
+						prop_value = elem1.attributes["value"]
+						f.print "#{prop_name} = #{prop_value}\n"
+
+					when "comment"
+						comment_value = elem1.attributes["value"]
+						f.print ";#{comment_value}\n"
+
+					else
+						raise "Unexpected #{elem1.name} element"
+					end
+				end
+
 			when "prop"
-				prop_name = elem.attributes["name"]
-				prop_value = elem.attributes["value"]
+				prop_name = elem0.attributes["name"]
+				prop_value = elem0.attributes["value"]
 				f.print "#{prop_name} = #{prop_value}\n"
 
 			when "comment"
-				comment_value = elem.attributes["value"]
+				comment_value = elem0.attributes["value"]
 				f.print ";#{comment_value}\n"
 
 			else
-				raise "Unexpected #{elem.name} element"
+				raise "Unexpected #{elem0.name} element"
 
 			end
 		end
