9ec4e35ae2e066bf068786675e6a657dd7dc80e7
James Pharaoh <james@phsys.co.uk>
2011-07-12 21:29:51 +0200

    added new json-lines file format without fancy spacing

--- ruby/mandar/support/json.rb
+++ ruby/mandar/support/json.rb
@@ -1,11 +1,20 @@
 module Mandar::Support::JSON
 
 	Mandar::Deploy::Formats.register self, :json
+	Mandar::Deploy::Formats.register self, :json_lines
 
 	def self.format_json file_elem, f
 		file_elem.find("*").each do |elem|
 			config = Mandar::Support::ConfigFuncs.xml_to_config elem
-			json = JSON.fast_generate config, :indent => "\t", :space => " ", :object_nl => "\n", :array_nl => "\n"
+			json = JSON.generate config, :indent => "\t", :space => " ", :object_nl => "\n", :array_nl => "\n"
+			f.puts json
+		end
+	end
+
+	def self.format_json_lines file_elem, f
+		file_elem.find("*").each do |elem|
+			config = Mandar::Support::ConfigFuncs.xml_to_config elem
+			json = JSON.generate config
 			f.puts json
 		end
 	end
