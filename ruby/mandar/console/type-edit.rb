cf8a75299837e5957e0ff5b84ab8e00b09b5ec24
James Pharaoh <james@phsys.co.uk>
2011-11-02 19:12:55 +0100

    make ts-update field show calendar and raw value

--- ruby/mandar/console/type-edit.rb
+++ ruby/mandar/console/type-edit.rb
@@ -280,7 +280,8 @@ class Mandar::Console::TypeEdit
 # ======================================== ts update field
 
 	def create_field_ts_update type_elem, field_elem, path, value, depth, readonly
-		return make_text_field path, field_elem.attributes["name"], to_ymd_hms(value), depth, true
+		value_str = "%s (%s)" % [ to_ymd_hms(value), value ]
+		return make_text_field path, field_elem.attributes["name"], value_str, depth, true
 	end
 
 	def update_field_ts_update type_elem, field_elem, path, form, value
