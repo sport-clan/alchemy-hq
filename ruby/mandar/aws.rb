65f06a957d3dc7155f735301491e17e704e95148
James Pharaoh <james@phsys.co.uk>
2012-06-01 19:02:19 +0200

    refactor security groups and use own aws client

--- ruby/mandar/aws.rb
+++ ruby/mandar/aws.rb
@@ -1,4 +1,27 @@
 module Mandar::AWS
+
+	def self.connect endpoint, access_key_id, secret_access_key, version
+
+		aws_account =
+			Mandar::AWS::Account.new
+
+		aws_account.access_key_id =
+			access_key_id
+
+		aws_account.secret_access_key =
+			secret_access_key
+
+		aws_client =
+			Mandar::AWS::Client.new \
+				aws_account,
+				endpoint,
+				version
+
+		aws_client.default_prefix = "a"
+
+		return aws_client
+	end
+
 end
 
 require "mandar/aws/account"
--- ruby/mandar/ec2/securitygroups.rb
+++ ruby/mandar/ec2/securitygroups.rb
@@ -1,120 +1,282 @@
 module Mandar::EC2::SecurityGroups
 
-	Mandar::Deploy::Commands.register self, :ec2_security_group
+	Mandar::Deploy::Commands.register self, :ec2_security_groups
 
-	LOCK = Mutex.new
-	LOCKS = {}
+	def self.command_ec2_security_groups groups_elem
 
-	def self.lock(name)
-		LOCK.synchronize do
-			return LOCKS[name] ||= Mutex.new
+		group_account_name =
+			groups_elem.attributes["account-name"]
+
+		group_region_name =
+			groups_elem.attributes["region-name"]
+
+		group_access_key_id =
+			groups_elem.attributes["access-key-id"]
+
+		group_secret_access_key =
+			groups_elem.attributes["secret-access-key"]
+
+		group_endpoint =
+			groups_elem.attributes["endpoint"]
+
+		account = {
+			access_key_id: group_access_key_id,
+			secret_access_key: group_secret_access_key,
+			endpoint: group_endpoint,
+		}
+
+		ec2_client =
+			Mandar::AWS.connect \
+				group_endpoint,
+				group_access_key_id,
+				group_secret_access_key,
+				"2012-05-01"
+
+		account_name =
+			"#{group_account_name}:#{group_region_name}"
+
+		Mandar.debug "fetching security groups for #{account_name}"
+
+		existing_groups_doc =
+			ec2_client.describe_security_groups
+
+		existing_groups =
+			decode_amazon_response \
+				existing_groups_doc
+
+		target_groups =
+			decode_groups_elem groups_elem
+
+		target_groups.each do |group_name, target_group|
+
+			existing_group =
+				existing_groups[group_name]
+
+			unless existing_group
+
+				create_security_group \
+					ec2_client,
+					account_name,
+					group_name,
+					target_group[:description]
+
+				existing_group = {}
+			end
+
+			update_security_group \
+				ec2_client,
+				account_name,
+				group_name,
+				existing_group,
+				target_group[:rules]
 		end
 	end
 
-	def self.command_ec2_security_group(group_elem)
+	def self.create_security_group \
+			ec2_client,
+			account_name,
+			group_name,
+			group_description
 
-		group_account = group_elem.attributes["account"]
-		group_name = group_elem.attributes["name"]
-		group_description = group_elem.attributes["description"]
+		Mandar.notice "creating security group #{account_name}/#{group_name}"
 
-		if $no_database
-			Mandar.debug "Skipping <ec2-security-group account=\"%s\" name=\"%s\">" %
-				[ group_account, group_name ]
-			return
+		unless $mock
+
+			result_doc =
+				ec2_client.create_security_group \
+					:group_name => group_name,
+					:group_description => group_description
+
+			query = "
+				/ a:CreateSecurityGroupResponse
+				/ a:return
+			"
+
+			result_doc.find_first(query).content == "true" \
+				or raise "Amazon error"
+
+		end
+	end
+
+	def self.decode_groups_elem groups_elem
+
+		ret = {}
+
+		groups_elem.find("*").each do |elem|
+
+			case elem.name
+
+				when "group"
+					group_elem = elem
+
+					group_name =
+						group_elem.attributes["name"]
+
+					group_description =
+						group_elem.attributes["description"]
+
+					ret[group_name] = {
+						name: group_name,
+						description: group_description,
+						rules: decode_group_elem(group_elem),
+					}
+
+				else
+					raise "Unexpected element #{elem0name}"
+			end
 		end
 
+		return ret
+	end
+
+	def self.decode_group_elem group_elem
+
 		rules = {}
+
 		group_elem.find("*").each do |elem0|
 			case elem0.name
 
-			when "allow"
-				allow_elem = elem0
+				when "allow"
+					allow_elem = elem0
 
-				allow_port = allow_elem.attributes["port"]
-				allow_protocol = allow_elem.attributes["protocol"]
-				allow_source = allow_elem.attributes["source"]
+					allow_port = allow_elem.attributes["port"]
+					allow_protocol = allow_elem.attributes["protocol"]
+					allow_source = allow_elem.attributes["source"]
 
-				if allow_protocol == "icmp"
-					allow_port_from = "-1"
-					allow_port_to = "-1"
-				elsif allow_port =~ /^([0-9]+)-([0-9]+)$/
-					raise "Not implemented"
-				elsif allow_port =~ /^([0-9]+)$/
-					allow_port_from = $1
-					allow_port_to = $1
-				elsif allow_port == nil || allow_port == ""
-					allow_port_from = "0"
-					allow_port_to = "65535"
+					if allow_protocol == "icmp"
+						allow_port_from = "-1"
+						allow_port_to = "-1"
+					elsif allow_port =~ /^([0-9]+)-([0-9]+)$/
+						raise "Not implemented"
+					elsif allow_port =~ /^([0-9]+)$/
+						allow_port_from = $1
+						allow_port_to = $1
+					elsif allow_port == nil || allow_port == ""
+						allow_port_from = "0"
+						allow_port_to = "65535"
+					else
+						raise "Invalid allow port: #{allow_port}"
+					end
+
+					if allow_source =~ /^([0-9]+\.[0-9]+\.[0-9]+\.[0-9]+)\/([0-9]+)/
+						allow_source = "#{$1}/#{$2}"
+					elsif allow_source =~ /^([0-9]+\.[0-9]+\.[0-9]+\.[0-9]+)/
+						allow_source = "#{$1}/32"
+					elsif allow_source == nil || allow_source == ""
+						allow_source = "0.0.0.0/0"
+					elsif allow_source =~ /^([a-z][-a-z0-9]*)$/
+						allow_source = "#{$1}"
+					elsif allow_source =~ /^([a-z][-a-z0-9]*)\/([a-z][-a-z0-9]*)$/
+						allow_source = "#{$1}/#{$2}"
+					else
+						raise "Invalid allow source: #{allow_source}"
+					end
+
+					key = {
+						:protocol => allow_protocol,
+						:from_port => allow_port_from,
+						:to_port => allow_port_to,
+						:source => allow_source,
+					}
+					rules[key] = true
+
 				else
-					raise "Invalid allow port: #{allow_port}"
-				end
-
-				if allow_source =~ /^([0-9]+\.[0-9]+\.[0-9]+\.[0-9]+)\/([0-9]+)/
-					allow_source = "#{$1}/#{$2}"
-				elsif allow_source =~ /^([0-9]+\.[0-9]+\.[0-9]+\.[0-9]+)/
-					allow_source = "#{$1}/32"
-				elsif allow_source == nil || allow_source == ""
-					allow_source = "0.0.0.0/0"
-				elsif allow_source =~ /^([a-z][-a-z0-9]*)$/
-					allow_source = "#{$1}"
-				elsif allow_source =~ /^([a-z][-a-z0-9]*)\/([a-z][-a-z0-9]*)$/
-					allow_source = "#{$1}/#{$2}"
-				else
-					raise "Invalid allow source: #{allow_source}"
-				end
-
-				key = {
-					:protocol => allow_protocol,
-					:from_port => allow_port_from,
-					:to_port => allow_port_to,
-					:source => allow_source,
-				}
-				rules[key] = true
-
-			else
-				raise "Unexpected element #{elem0.name}"
+					raise "Unexpected element #{elem0.name}"
 			end
 		end
 
-		update_security_group group_account.to_s, group_name.to_s, group_description.to_s, rules
+		return rules
 	end
 
-	def self.init_security_groups(account_name)
+	def self.init_security_groups \
+			ec2_client,
+			account_name
+
 		self.lock(account_name).synchronize do
+
 			@security_groups ||= {}
+
 			unless @security_groups[account_name]
-				Mandar.debug "fetching security groups for #{account_name}"
-				ec2 = Mandar::EC2.connect(account_name)
-				@security_groups[account_name] = {}
-				res = ec2.describe_security_groups
-				res.securityGroupInfo.item.each do |res_sec_group|
-					res_sec_group_name = res_sec_group.groupName
-					@security_groups[account_name][res_sec_group_name] = {}
-					next unless res_sec_group.ipPermissions
-					res_sec_group.ipPermissions.item.each do |res_perm|
-						key = {
-							:protocol => res_perm.ipProtocol,
-							:from_port => res_perm.fromPort,
-							:to_port => res_perm.toPort,
-						}
-						if res_perm.groups
-							res_perm.groups.item.each do |res_perm_group|
-								temp_key = key.clone
-								temp_key[:source] = res_perm_group.groupName
-								@security_groups[account_name][res_sec_group_name][temp_key] = true
-							end
-						end
-						if res_perm.ipRanges
-							res_perm.ipRanges.item.each do |res_perm_range|
-								temp_key = key.clone
-								temp_key[:source] = res_perm_range.cidrIp
-								@security_groups[account_name][res_sec_group_name][temp_key] = true
-							end
-						end
-					end
+
+			end
+		end
+	end
+
+	def self.decode_amazon_response groups_doc
+
+		ret = {}
+
+		query = "
+			/ a:DescribeSecurityGroupsResponse
+			/ a:securityGroupInfo
+			/ a:item
+		"
+
+		groups_doc.find(query).each do |group_elem|
+
+			group_name =
+				group_elem.find_first("a:groupName").content
+
+			ret[group_name] = {}
+
+			query = "
+				a:ipPermissions
+				/ a:item
+			"
+
+			group_elem.find(query).each do |perm_elem|
+
+				key = {
+
+					:protocol => perm_elem \
+						.find_first("a:ipProtocol").content,
+
+					:from_port => perm_elem \
+						.find_first("a:fromPort").content,
+
+					:to_port => perm_elem \
+						.find_first("a:toPort").content,
+				}
+
+				query = "
+					a:groups
+					/ a:item
+				"
+
+				perm_elem.find(query).each do |perm_group_elem|
+
+					temp_key = key.clone
+
+					temp_key[:source] =
+						perm_group_elem \
+							.find_first("a:groupName") \
+							.content
+
+					ret [group_name] [temp_key] = true
+
+				end
+
+				query = "
+					a:ipRanges
+					/ a:item
+				"
+
+				perm_elem.find(query).each do |perm_range_elem|
+
+					temp_key = key.clone
+
+					temp_key[:source] =
+						perm_range_elem \
+							.find_first("a:cidrIp") \
+							.content
+
+					ret [group_name] [temp_key] = true
+
 				end
 			end
 		end
+
+		return ret
 	end
 
 	def self.rule_port_str(rule)
@@ -131,46 +293,33 @@ module Mandar::EC2::SecurityGroups
 		end
 	end
 
-	def self.update_security_group(account_name, group_name, group_description, new_rules)
-
-		# fetch data from amazon
-		init_security_groups account_name
-
-		# setup ec2 connection
-		ec2 = Mandar::EC2.connect(account_name)
-
-		# make sure security group exists
-		unless @security_groups[account_name][group_name]
-			Mandar.notice "creating security group #{account_name}/#{group_name}"
-			unless $mock
-				result = ec2.create_security_group({
-					:group_name => group_name,
-					:group_description => group_description,
-				})
-				raise "Amazon error" unless result.return == "true"
-			end
-			@security_groups[account_name][group_name] = {}
-		end
-
-		# see if anything needs changing
-		old_rules = @security_groups[account_name][group_name]
-		return if old_rules == new_rules
+	def self.update_security_group \
+			ec2_client,
+			account_name,
+			group_name,
+			old_rules,
+			new_rules
 
 		# find rules to add and remove
+
 		changes = []
+
 		(new_rules.keys - old_rules.keys).each do |rule|
 			changes << { :type => :add, :rule => rule }
 		end
+
 		(old_rules.keys - new_rules.keys).each do |rule|
 			changes << { :type => :remove, :rule => rule }
 		end
 
 		# process changes
+
 		changes.each do |change|
 			type = change[:type]
 			rule = change[:rule]
 
 			# output a message
+
 			info = "%s: %s port %s from %s" % [
 				"#{account_name}/#{group_name}",
 				rule[:protocol],
@@ -180,35 +329,72 @@ module Mandar::EC2::SecurityGroups
 			Mandar.notice "#{type} firewall rule #{info}"
 
 			# prepare amazon request
+
 			options = {
 				:group_name => group_name,
 				:ip_protocol => rule[:protocol],
 				:from_port => rule[:from_port],
 				:to_port => rule[:to_port],
 			}
+
 			if rule[:source] =~ /^[0-9]+\.[0-9]+\.[0-9]+\.[0-9]+\/[0-9]+/
+
 				options[:cidr_ip] = rule[:source]
+
 			elsif rule[:source] =~ /^[a-z]+(?:-[a-z]+)*$/
+
+				rule[:from_port] == "-1" \
+					or raise "Error"
+
+				rule[:to_port] == "-1" \
+					or raise "Error"
+
 				options[:source_security_group_name] = rule[:source]
+
 			else
 				raise "invalid rule source: #{rule[:source]}"
 			end
 
 			# perform amazon request
+
 			unless $mock
+
 				case type
-				when :add
-					result = ec2.authorize_security_group_ingress options
-				when :remove
-					result = ec2.revoke_security_group_ingress options
+
+					when :add
+
+						result_doc =
+							ec2_client.authorize_security_group_ingress \
+								options
+
+						query = "
+							/ a:AuthorizeSecurityGroupIngressResponse
+							/ a:return
+						"
+
+						result_doc.find_first(query).content == "true" \
+							or raise "Amazon error"
+
+					when :remove
+
+						result =
+							ec2_client.revoke_security_group_ingress \
+								options
+
+						query = "
+							/ a:RevokeSecurityGroupIngressResponse
+							/ a:return
+						"
+
+						result_doc.find_first(query).content == "true" \
+							or raise "Amazon error"
+
 				end
-				raise "Amazon error" unless result.return == "true"
+
 			end
 
 		end
 
-		# update state
-		@security_groups[account_name][group_name] = new_rules
 	end
 
 end
