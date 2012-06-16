aea368d47cfcea5b342c7057e7ba7c34101a5d1d
James Pharaoh <james@phsys.co.uk>
2012-06-16 19:08:11 +0200

    make apt-remove ignore "provided" packages

--- ruby/mandar/support/debian.rb
+++ ruby/mandar/support/debian.rb
@@ -1,65 +1,114 @@
 module Mandar::Debian
 
-	def self.apt_packages(force = false)
-		return @apt_packages if @apt_packages && ! force
+	def self.init_packages force = false
+
+		return if @ready unless force
+
 		apt_packages = []
+		apt_provides = []
+
 		%x[
 			dpkg-query -W --showformat '${Status}:${Package}:${Provides}\n'
 		].split("\n").each do |line|
 			status, package, provides = line.split ":"
 			next unless status == "install ok installed"
 			apt_packages += package.split ", "
-			apt_packages += provides.split ", " if provides
+			apt_provides += provides.split ", " if provides
 		end
-		return @apt_packages = apt_packages
+
+		@apt_packages = apt_packages
+		@apt_provides = apt_provides
+		@ready = true
+
 	end
 
-	def self.apt_install(*names)
+	def self.apt_packages force = false
+		init_packages force
+		return @apt_packages
+	end
 
-		names_to_install = names - apt_packages
+	def self.apt_provides force = false
+		init_packages force
+		return @apt_provides
+	end
+
+	def self.apt_install *names
+
+		names_to_install = names - apt_packages - apt_provides
+
 		return if names_to_install.empty?
 
 		names_to_install_str = names_to_install.join ' '
 
 		Mandar.detail "updating package cache"
+
 		full_cmd = "apt-get -y update"
-		Mandar::Support::Core.shell full_cmd or raise "Error executing #{full_cmd}" unless $mock
+
+		unless $mock
+			Mandar::Support::Core.shell full_cmd \
+				or raise "Error executing #{full_cmd}"
+		end
 
 		Mandar.notice "installing #{names_to_install_str}"
-		apt_cmd = Mandar.shell_quote(%W[ apt-get -y install ] + names_to_install)
-		full_cmd = "DEBIAN_FRONTEND=noninteractive #{apt_cmd}"
-		Mandar::Support::Core.shell full_cmd or raise "Error executing #{full_cmd}" unless $mock
 
-		apt_packages(true)
+		apt_cmd = Mandar.shell_quote(%W[ apt-get -y install ] + names_to_install)
+
+		full_cmd = "DEBIAN_FRONTEND=noninteractive #{apt_cmd}"
+
+		unless $mock
+			Mandar::Support::Core.shell full_cmd \
+				or raise "Error executing #{full_cmd}"
+		end
+
+		@ready = false
 	end
 
-	def self.apt_remove(*names)
+	def self.apt_remove *names
 
 		names_to_remove = names & apt_packages
+
 		return if names_to_remove.empty?
 
 		names_to_remove_str = names_to_remove.join ' '
 
 		Mandar.detail "updating package cache"
-		system "apt-get -q2 update" or raise "Error" unless $mock
+
+		unless $mock
+			system "apt-get -q2 update" \
+				or raise "Error"
+		end
 
 		Mandar.notice "remove #{names_to_remove_str}"
-		apt_cmd = Mandar.shell_quote(%W[ apt-get -y remove ] + names_to_remove)
-		system "#{apt_cmd}" or raise "Error" unless $mock
 
-		apt_packages(true)
+		apt_cmd =
+			Mandar.shell_quote \
+				%W[ apt-get -y remove ] + names_to_remove
+
+		unless $mock
+			system "#{apt_cmd}" \
+				or raise "Error"
+		end
+
+		@ready = false
 	end
 
-	# manage debconf entries
-	def self.debconf_set_selections(*entries)
+	def self.debconf_set_selections *entries
+
 		unless @debconf
+
 			@debconf = {}
 			apt_install "debconf-utils"
 			a = Time.now
-			`debconf-get-selections`.split("\n").select { |s| s !~ /^#/ }.map { |s| s.split("\t", 4) }.each do |line|
-				package, question, type, value = line
-				@debconf[question] = { :type => type, :value => value }
-			end
+
+			`debconf-get-selections` \
+				.split("\n") \
+				.select { |s| s !~ /^#/ } \
+				.map { |s| s.split("\t", 4) } \
+				.each do |line|
+					package, question, type, value = line
+					@debconf[question] = { :type => type, :value => value }
+				end
+
 			b = Time.now
 			Mandar.debug "debconf-get-selections took #{((b-a)*1000).to_i}ms"
 		end
@@ -88,11 +137,14 @@ module Mandar::Debian
 	end
 
 	# interface to debian's dpkg-statoverride command
-	def self.dpkg_statoverride(user, group, mode, path)
-		uid = Mandar::Support::Core.to_uid(user)
-		gid = Mandar::Support::Core.to_gid(group)
+
+	def self.dpkg_statoverride user, group, mode, path
+
+		uid = Mandar::Support::Core.to_uid user
+		gid = Mandar::Support::Core.to_gid group
 
 		# initialise list of existing overrides
+
 		unless @statoverrides
 			@statoverrides = {}
 			`dpkg-statoverride --list`.split("\n").each do |line|
@@ -106,6 +158,7 @@ module Mandar::Debian
 		end
 
 		# do nothing if no changes
+
 		statoverride = @statoverrides[path]
 		return false if statoverride &&
 			statoverride[:user] == user &&
@@ -113,16 +166,22 @@ module Mandar::Debian
 			statoverride[:mode] == mode
 
 		# output a message
+
 		Mandar.notice "setting permissions for #{path}"
 		Mandar::Deploy::Flag.auto
 
 		# remove existing
+
 		system "dpkg-statoveride --remote #{path}" if statoverride unless $mock
 
 		# add new
-		system "dpkg-statoverride --add --update #{user} #{group} 0#{mode.to_s(8)} #{path}" unless $mock
+
+		unless $mock
+			system "dpkg-statoverride --add --update #{user} #{group} 0#{mode.to_s(8)} #{path}"
+		end
 
 		# update list
+
 		@statoverrides[path] = {
 			:user => user,
 			:group => group,
@@ -132,23 +191,38 @@ module Mandar::Debian
 		return true
 	end
 
-	def self.runlevel_update(service, *start_levels)
+	def self.runlevel_update service, *start_levels
+
 		start_levels.flatten!
 		start_levels.map! { |level| level.to_i }
-		raise "No such service: #{service}" unless File.exists? "/etc/init.d/#{service}"
+
+		File.exists? "/etc/init.d/#{service}" \
+			or raise "No such service: #{service}"
+
 		Dir.glob("/etc/rc?.d").each do |rc_dir|
+
 			level = rc_dir[7...8].to_i
+
 			Dir.glob("#{rc_dir}/[SK][0-9][0-9]#{service}").each do |rc_file|
+
 				old_status = rc_file[11...12]
 				new_status = start_levels.include?(level) ? "S" : "K"
+
 				next if old_status == new_status
+
 				new_file = rc_file.clone
 				new_file[11] = new_status
+
 				Mandar.notice "renaming #{rc_file} to #{new_file}"
+
 				FileUtils.mv rc_file, new_file unless $mock
+
 				Mandar::Deploy::Flag.auto
+
 			end
+
 		end
+
 	end
 
 	Mandar::Deploy::Commands.register self, :apt
@@ -160,7 +234,7 @@ module Mandar::Debian
 	Mandar::Deploy::Commands.register self, :update_enabled
 	Mandar::Deploy::Commands.register self, :update_rcd
 
-	def self.command_apt(apt_elem)
+	def self.command_apt apt_elem
 
 		apt_packages = apt_elem.attributes["packages"]
 		names = apt_packages.split
@@ -168,21 +242,21 @@ module Mandar::Debian
 		apt_install *names
 	end
 
-	def self.command_apt_install(apt_elem)
+	def self.command_apt_install apt_elem
 
 		apt_package = apt_elem.attributes["package"]
 
 		apt_install *apt_package
 	end
 
-	def self.command_apt_remove(apt_elem)
+	def self.command_apt_remove apt_elem
 
 		apt_package = apt_elem.attributes["package"]
 
 		apt_remove *apt_package
 	end
 
-	def self.command_debconf(debconf_elem)
+	def self.command_debconf debconf_elem
 
 		debconf_package = debconf_elem.attributes["package"]
 		debconf_question = debconf_elem.attributes["question"]
@@ -197,25 +271,42 @@ module Mandar::Debian
 		})
 	end
 
-	def self.command_runlevel_update(runlevel_update_elem)
+	def self.command_runlevel_update runlevel_update_elem
 
-		runlevel_update_service = runlevel_update_elem.attributes["service"]
-		runlevel_update_levels = runlevel_update_elem.attributes["levels"]
+		runlevel_update_service =
+			runlevel_update_elem.attributes["service"]
 
-		runlevel_update runlevel_update_service, runlevel_update_levels.scan(/./)
+		runlevel_update_levels =
+			runlevel_update_elem.attributes["levels"]
+
+		runlevel_update \
+			runlevel_update_service, \
+			runlevel_update_levels.scan(/./)
 	end
 
-	def self.command_statoverride(statoverride_elem)
+	def self.command_statoverride statoverride_elem
 
-		statoverride_name = statoverride_elem.attributes["name"]
-		statoverride_user = statoverride_elem.attributes["user"]
-		statoverride_group = statoverride_elem.attributes["group"]
-		statoverride_mode = statoverride_elem.attributes["mode"]
+		statoverride_name =
+			statoverride_elem.attributes["name"]
 
-		dpkg_statoverride statoverride_user, statoverride_group, statoverride_mode.to_i(8), statoverride_name
+		statoverride_user =
+			statoverride_elem.attributes["user"]
+
+		statoverride_group =
+			statoverride_elem.attributes["group"]
+
+		statoverride_mode =
+			statoverride_elem.attributes["mode"]
+
+		dpkg_statoverride \
+			statoverride_user,
+			statoverride_group,
+			statoverride_mode.to_i(8),
+			statoverride_name
+
 	end
 
-	def self.command_update_rcd(ur_elem)
+	def self.command_update_rcd ur_elem
 
 		ur_name = ur_elem.attributes["name"]
 
@@ -224,7 +315,11 @@ module Mandar::Debian
 		Mandar.notice "creating /etc/rc?.d links for #{ur_name}"
 		Mandar::Deploy::Flag.auto
 
-		system "update-rc.d #{ur_name} defaults" or raise "Error" unless $mock
+		unless $mock
+			system "update-rc.d #{ur_name} defaults" \
+				or raise "Error"
+		end
+
 	end
 
 end
