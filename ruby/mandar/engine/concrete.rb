cbbe90c7808365a47c123e79e535d297219e7583
James Pharaoh <james@phsys.co.uk>
2012-06-19 12:50:58 +0200

    tidy up

--- ruby/mandar/engine/concrete.rb
+++ ruby/mandar/engine/concrete.rb
@@ -9,22 +9,38 @@ module Mandar::Engine::Concrete
 		start_time = Time.now
 
 		Dir.new("#{CONFIG}/concrete").each do |filename|
-			next unless filename =~ /^([a-z0-9]+(?:-[a-z0-9]+)*)\.(xslt|xquery)$/
+
+			filename_regex =
+				/^([a-z0-9]+(?:-[a-z0-9]+)*)\.(xslt|xquery)$/
+
+			next unless filename =~ filename_regex
+
 			Mandar.trace "loading #{filename}"
+
 			concrete = {}
+
 			concrete[:name] = $1
 			concrete[:type] = $2
 			concrete[:path] = "#{CONFIG}/concrete/#{filename}"
 			concrete[:source] = File.read concrete[:path]
 			concrete[:in] = []
-			concrete[:source].scan(/\(: (in) ([a-z0-9]+(?:-[a-z0-9]+)*) :\)$/).each do |type, name|
+
+			decl_regex =
+				/\(: (in) ([a-z0-9]+(?:-[a-z0-9]+)*) :\)$/
+
+			concrete[:source].scan(decl_regex).each do |type, name|
 				concrete[type.to_sym] << name
 			end
+
 			concretes[concrete[:name]] = concrete
+
 		end
 
 		end_time = Time.now
-		Mandar.trace "loading concrete sources took #{((end_time - start_time) * 1000).to_i}ms"
+
+		time_ms = ((end_time - start_time) * 1000).to_i
+
+		Mandar.trace "loading concrete sources took #{time_ms}ms"
 
 		return @concretes = concretes
 	end
@@ -48,8 +64,11 @@ module Mandar::Engine::Concrete
 
 			# create directories
 
-			FileUtils.remove_entry_secure "#{WORK}/concrete" if File.directory? "#{WORK}/concrete"
+			FileUtils.remove_entry_secure "#{WORK}/concrete" \
+				if File.directory? "#{WORK}/concrete"
+
 			FileUtils.mkdir "#{WORK}/concrete"
+
 			hosts.each do |host|
 				FileUtils.mkdir "#{WORK}/concrete/#{host}"
 			end
@@ -57,6 +76,7 @@ module Mandar::Engine::Concrete
 			# load xquery modules
 
 			include_dir = "#{CONFIG}/include"
+
 			Dir["#{include_dir}/*.xquery"].each do |path|
 				path =~ /^ #{Regexp.quote include_dir} \/ (.+) $/x
 				name = $1
@@ -73,14 +93,19 @@ module Mandar::Engine::Concrete
 				#xquery = Mandar::Engine.zorba.compileQuery(concrete[:source])
 
 				# setup abstract
+
 				doc = XML::Document.new
 				doc.root = XML::Node.new "abstract"
+
 				concrete[:in].each do |abstract_name|
 					unless result = abstract_results[abstract_name]
-						Mandar.debug "No abstract result for #{abstract_name}, requested by #{concrete_name}"
+						Mandar.debug "No abstract result for " +
+							"#{abstract_name}, requested by #{concrete_name}"
 						next
 					end
-					result[:doc].root.each { |elem| doc.root << doc.import(elem) }
+					result[:doc].root.each do |elem|
+						doc.root << doc.import(elem)
+					end
 				end
 
 				case concrete[:type]
@@ -128,7 +153,8 @@ module Mandar::Engine::Concrete
 
 				hosts.each do |host|
 
-					Mandar.debug "rebuilding concrete config #{concrete_name} for #{host}"
+					Mandar.debug "rebuilding concrete config " +
+						"#{concrete_name} for #{host}"
 
 					# set hostname
 
@@ -174,7 +200,11 @@ module Mandar::Engine::Concrete
 
 					# save doc
 
-					doc = XML::Document.string ret, :options => XML::Parser::Options::NOBLANKS
+					doc =
+						XML::Document.string \
+							ret,
+							:options => XML::Parser::Options::NOBLANKS
+
 					doc.save "#{WORK}/concrete/#{host}/#{concrete_name}.xml"
 
 				end
@@ -204,7 +234,10 @@ module Mandar::Engine::Concrete
 		end
 
 		end_time = Time.now
-		Mandar.trace "rebuilding concrete config took #{((end_time - start_time) * 1000).to_i}ms"
+
+		time_ms = ((end_time - start_time) * 1000).to_i
+
+		Mandar.trace "rebuilding concrete config took #{time_ms}ms"
 
 	end
 
